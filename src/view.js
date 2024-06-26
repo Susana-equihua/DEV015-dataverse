ce26c1bb8f8fd49da1b57d341b62b6930734f865
susana-equihua <susiequihua@gmail.com>
2024-06-26 16:17:16 -0600

    Primera visualización en view y comienzo de la estructura html

--- src/view.js
+++ src/view.js
@@ -1,17 +1,19 @@
 export const renderItems = (data) => {
   console.log(data)
   const ul = document.createElement("ul"); //Crear un <ul> con metodo createElement; se guarda en una variable para después ser utilizada
+  ul.setAttribute("class","flex-container"); //Se le agrega un atributo id para que sea el contenedor flex
   data.forEach(function(data){   //Metodo forEach o map, para recorrer el arreglo de objetos
     console.log(data) //imprimir en la consola; aparece un listado detallado por objeto
     const li = document.createElement("li"); //Crear un <li> por cada objeto de la data
     console.log(li);
+    li.setAttribute("class","flex-items")
     ul.appendChild(li);//Agregar cada <li> al <ul> con appenChild u otro metodo apropiado
     console.log(ul);
     const div = document.querySelector('div[id=tarjetas]'); //Crear un elemento HTML y llamarlo con un selector del DOM
     div.appendChild(ul);  //Situar la lista no ordenada (ul) dentro de la etiqueta div
     //li.innerHTML = data; //Renderizar la base de datos de los personajes, solo aparece [Object object]
     li.innerHTML = `
-    <img src = ${data.imageUrl}>  
+    <img src = ${data.imageUrl} alt = ${data.name}>  
     <h3 id= "title"> Mascota: ${data.name} </h3>
     <p> Descripción: ${data.shortDescription} </p>
     <p> Especie: ${data.facts.species} </p>

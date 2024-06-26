616fe5b2279e654a43b58af976f47addbacf5fe9
Pamelajharely83 <pamelajharely83@gmail.com>
2024-06-26 14:34:09 -0500

    Actualización de view.js y avancé de css

--- src/view.js
+++ src/view.js
@@ -1,30 +1,34 @@
 export const renderItems = (data) => {
   console.log(data)
-  const ul = document.createElement("ul"); //Crear un <ul> con metodo createElement; se guarda en una variable para después ser utilizada
+  const elementUl = document.createElement("ul"); //Crear un <ul> con metodo createElement; se guarda en una variable para después ser utilizada
+  elementUl.setAttribute("class","flex-container");
   data.forEach(function(data){   //Metodo forEach o map, para recorrer el arreglo de objetos
     console.log(data) //imprimir en la consola; aparece un listado detallado por objeto
-    const li = document.createElement("li"); //Crear un <li> por cada objeto de la data
-    console.log(li);
-    ul.appendChild(li);//Agregar cada <li> al <ul> con appenChild u otro metodo apropiado
-    console.log(ul);
-    const div = document.querySelector('div[id=tarjetas]'); //Crear un elemento HTML y llamarlo con un selector del DOM
-    div.appendChild(ul);  //Situar la lista no ordenada (ul) dentro de la etiqueta div
+    const elementLi = document.createElement("li"); //Crear un <li> por cada objeto de la data
+    elementLi.setAttribute("class", "item-flex");
+    console.log(elementLi);
+    elementUl.appendChild(elementLi);//Agregar cada <li> al <ul> con appenChild u otro metodo apropiado
+    console.log(elementUl);
+    const elementSection = document.querySelector('section[id="grupoTarjetas"]'); //Crear un elemento HTML y llamarlo con un selector del DOM
+    elementSection.appendChild(elementUl);  //Situar la lista no ordenada (ul) dentro de la etiqueta div
     //li.innerHTML = data; //Renderizar la base de datos de los personajes, solo aparece [Object object]
-    li.innerHTML = `
-    <img src = ${data.imageUrl}>  
-    <h3 id= "title"> Mascota: ${data.name} </h3>
-    <p> Descripción: ${data.shortDescription} </p>
-    <p> Especie: ${data.facts.species} </p>
-    <p> Vive en: ${data.facts.livesIn} </p>
-    <p> Genero de la película: ${data.facts.filmGenre} </p>
-    <h5> Dato curioso:
-    <p> ${data.extraInfo.curiousFact} </p>
+    elementLi.innerHTML = `
+    <div id="text-card">
+    <div id="text">
+    <h3 id= "title">${data.name}</h3>
+    <p class="parrafoTarjetas"><span class="spanColor">Descripción: </span> ${data.shortDescription} </p>
+    <p class="parrafoTarjetas"><span class="spanColor">Especie: </span> ${data.facts.species} </p>
+    <p class="parrafoTarjetas"><span class="spanColor">Vive en: </span> ${data.facts.livesIn} </p>
+    <p class="parrafoTarjetas"><span class="spanColor">Genero de la película: </span> ${data.facts.filmGenre} </p>
+    <p class="parrafoTarjetas"><span class="fun-fact">Dato curioso:</span> ${data.extraInfo.curiousFact} </p>
+    </div>
+    <div id="divFondoPersonaje">
+    <img class="imgPersonaje" src = ${data.imageUrl} alt = "Aquí estaba ${data.name}">  
+    </div>
+    </div>
     `
   }); 
 
-  return ul   //Retornar el elemento <ul>
-
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  // return 'example';
+  return elementUl   //Retornar el elemento <ul>
 };
 
